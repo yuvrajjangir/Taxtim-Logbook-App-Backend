0f1a519cf8494549739ef17f82cfe4ac9491aa53
yuvrajjangir <yuvijangir78@gmail.com>
2024-01-24 21:17:30 +0530

    done

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const {UserModel} = require("../model/authmodel");
 const {Router} = require("express");
 const userController = Router();
